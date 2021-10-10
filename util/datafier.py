66869061979fd2a5eee4b82ff44714c1dfd48e13
Violet Moore <Pyroan@users.noreply.github.com>
2021-10-10 14:58:26 -0700

    add utils; json schema!

---
+++ util/datafier.py
@@ -0,0 +1,9 @@
+# Converts data from our databse into an open json format,
+# so people can run their own data analysis and stuff for Nerd Fun
+import json
+import models
+from jsonschema import validate
+
+
+def datafy() -> str:
+    pass
